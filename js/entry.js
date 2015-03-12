5b59c8ef443c340adb791771bc89d2d2c950d7e9
Erlend Røsjø <erlend@erlend.net>
2015-03-12 13:15:40 +0100

    Basic triangle

--- js/entry.js
+++ js/entry.js
@@ -1,7 +1,45 @@
 (function(){
   var board = JXG.JSXGraph.initBoard('box', {
     boundingbox: [-10, 10, 10, -10],
-    axis: true
+    axis: true,
+    showCopyright: false,
   });
-  console.log(board);
+
+  var pointA = [2,8];
+  var pointB = [-5,1];
+  var pointC = [5,0];
+
+  //Add points for triangle
+  board.create('point', pointA, {
+    name: 'A',
+    strokecolor: 'red',
+  });
+
+  board.create('point', pointB, {
+    name: 'B',
+    strokecolor: 'red',
+  });
+
+  board.create('point', pointC, {
+    name: 'C',
+    strokecolor: 'red',
+  });
+
+  var lineAB = board.create('line', [pointA, pointB], {
+    straightFirst: false,
+    straightLast: false, 
+    strokeWidth: 2,
+  });
+
+  var lineAC = board.create('line', [pointA, pointC], {
+    straightFirst: false,
+    straightLast: false, 
+    strokeWidth: 2,
+  });
+
+  var lineBC = board.create('line', [pointB, pointC], {
+    straightFirst: false,
+    straightLast: false, 
+    strokeWidth: 2,
+  });
 })();
